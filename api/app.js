9e56a832dfce753f7c59e8736900509be8a9a395
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-12-28 00:00:14 -0300

    feat: Adiciona rota /add-user e inclui acesso à API com cors

--- api/app.js
+++ api/app.js
@@ -3,26 +3,36 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI.js");
+var testAPIRouter = require("./routes/testAPI");
+var addUserRouter = require("./routes/addUser");
 
 var app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  app.use(cors());
+  next();
+});
+app.use(cors({ origin: true, credentials: true }));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/add-user", addUserRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
