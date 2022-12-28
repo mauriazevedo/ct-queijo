c7eefa9484a7855918c4b51ee6828b76d1475dfb
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-12-27 23:57:08 -0300

    feat: Cria código para criação de tabela de usuários

---
+++ api/db/users/users.create-table.js
@@ -0,0 +1,18 @@
+function CreateTable(conn) {
+  const sql =
+    "CREATE TABLE IF NOT EXISTS users (" +
+    "`user_id` INT NOT NULL AUTO_INCREMENT," +
+    "`user_name` VARCHAR(32) NOT NULL," +
+    "`user_last_name` VARCHAR(128) NOT NULL," +
+    "`user_organization` VARCHAR(128) NOT NULL," +
+    "`user_email` VARCHAR(128) NOT NULL UNIQUE," +
+    "`user_passwod` VARCHAR(512) NOT NULL," +
+    "`user_type` VARCHAR(4) NULL DEFAULT 'user'," +
+    "PRIMARY KEY (`user_id`)" +
+    ");";
+
+  conn.query(sql, (error, results, fields) => {
+    if (error) return console.log("deu pau" + error);
+    console.log("Tabela de usuários criada!");
+  });
+}
