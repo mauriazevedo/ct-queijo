f95423aeb96006806d3a208ddfbcaecd835feb4e
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-12-27 22:14:54 -0300

    feat: Inclui envio de dados para o banco

--- client/src/components/FormComponent/index.js
+++ client/src/components/FormComponent/index.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { AddUser } from "../../services/users";
 import { Form } from "./style";
 
 function FormComponent({ inputs, title }) {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      name: event.target.elements.name?.value,
+      lastname: event.target.elements.lastanme?.value,
+      organization: event.target.elements.organization?.value,
+      email: event.target.elements.email?.value,
+      password: event.target.elements.password?.value,
+    };
+
+    AddUser(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <h2>{title}</h2>
       {inputs.map((item, index) => {
         return (
@@ -13,6 +28,7 @@ function FormComponent({ inputs, title }) {
             placeholder={item.placeholder}
             value={item.value}
             name={item.name}
+            required={true}
           />
         );
       })}
