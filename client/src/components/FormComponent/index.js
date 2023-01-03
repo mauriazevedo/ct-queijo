27e820753a36dfc9b7c89599186cde0874187b3a
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2023-01-03 00:40:03 -0300

    Feat: Adiciona validação de dados com base no tipo de formulario (login ou cadastro)

--- client/src/components/FormComponent/index.js
+++ client/src/components/FormComponent/index.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { AddUser } from "../../services/users";
+import { AddUser, UserLogin } from "../../services/users";
 import { Form } from "./style";
 
 function FormComponent({ inputs, title }) {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const data = {
-      name: event.target.elements.name?.value,
-      lastname: event.target.elements.lastanme?.value,
-      organization: event.target.elements.organization?.value,
-      email: event.target.elements.email?.value,
-      password: event.target.elements.password?.value,
-    };
+    if (title == "Cadastre-se") {
+      const data = {
+        name: event.target.elements.name?.value,
+        lastname: event.target.elements.lastname?.value,
+        organization: event.target.elements.organization?.value,
+        email: event.target.elements.email?.value,
+        password: event.target.elements.password?.value,
+      };
 
-    AddUser(data);
+      console.log(await AddUser(data));
+    } else if (title == "Login") {
+      const data = {
+        email: event.target.elements.email?.value,
+        password: event.target.elements.password?.value,
+      };
+
+      console.log(await UserLogin(data));
+    }
   };
 
   return (
