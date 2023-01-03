f0e4e803a75c77e701f9ca6909bd64b048c06dc5
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2023-01-03 00:36:12 -0300

    feat: Cria rota de login

---
+++ api/routes/login.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+const encryptPassword = require("encrypt-password");
+const Login = require("../db/users/users.login");
+
+router.post("/", function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const hash = encryptPassword(password, "hs278ty817jsh");
+
+  Login(email, hash, res);
+});
+
+module.exports = router;
