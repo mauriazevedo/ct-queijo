bee869c9f0e5d98027d60999327111ed1957f01b
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2023-01-11 14:35:16 -0300

    feat: Habilita requisições patch

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ var app = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
