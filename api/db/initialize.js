148c5a5e04df2ca263133706c178a8112f8a86dd
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2023-01-11 18:25:02 -0300

    refactor: Insere código dentro de função assíncrona

--- api/db/initialize.js
+++ api/db/initialize.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2");
 const CreateOrdersTable = require("./orders/orders.createTable");
 const CreateUsersTable = require("./users/users.createTable");
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "ctqueijo",
-});
 
-CreateUsersTable(connection);
-CreateOrdersTable(connection);
+async function Initialize() {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "ctqueijo",
+  });
 
-return;
+  CreateUsersTable(connection);
+  CreateOrdersTable(connection);
+}
+
+module.exports = Initialize;
