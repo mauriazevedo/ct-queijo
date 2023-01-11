c1d715c2fe3b6b9dcee8d827a49934e610d5299b
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2023-01-11 14:36:35 -0300

    feat: Altera retorno para todos os campos da tabela

--- api/db/users/users.getUser.js
+++ api/db/users/users.getUser.js
@@ -1,7 +1,7 @@
 const execSQLQuery = require("../connection");
 
 async function GetUser(id, res) {
-  const query = `SELECT user_name, user_email, user_organization FROM users WHERE user_id = '${id}'`;
+  const query = `SELECT * FROM users WHERE user_id = '${id}'`;
 
   execSQLQuery(query, res);
 }
